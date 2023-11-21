78947e94a08e26761f1041ca672fecaa25963cf2
slevin48 <yann.debray@gadz.org>
2023-11-21 18:55:58 -0500

    Create article and add text

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,22 @@ from pathlib import Path
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 voices = ['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer']
 
+# Initialize session state
+if "create_article" not in st.session_state:
+    st.session_state.create_article = False
+if "edit_article" not in st.session_state:
+    st.session_state.edit_article = False
+if "text" not in st.session_state:
+    st.session_state.text = ''
+
 # Functions
+def create_article(title):
+    # Create text file without any content
+    with open(f'podcast/{episode}/text/{title}.txt', 'w', encoding='utf-8'):
+        pass
+    # with st.spinner('Wait for it...') as spinner:
+    #     st.write("Article created.")
+
 def summarize(text):
     inst = '''Resume en moins de 1000 caracteres:''' 
     completion = openai.chat.completions.create(
@@ -38,16 +53,41 @@ episode = 'episode' + '{:03d}'.format(episode_number)
 # Display articles
 directory = 'podcast/' + episode 
 if os.path.exists(directory):
+    if st.button("Create Article"):
+        st.session_state.create_article = True
+    if st.session_state.create_article:
+        if title := st.text_input("Enter Article title", key='title'):
+            # st.write("Article title: ", title)
+            create_article(title)
+            st.session_state.create_article = False
     articles = os.listdir(directory + '/text')
-    article = st.selectbox("Select an Article", articles)
-    
+    if ("title" in locals() or "title" in globals()) and title != '':
+        article_index = articles.index(title + '.txt')
+    else:
+        article_index = 0
+    article = st.selectbox("Select an Article", articles, index=article_index)
+
+    # Text
+    with open(f'podcast/{episode}/text/{article}', 'r', encoding='utf-8') as f:
+        text = f.read()
+    if st.toggle("Edit Article", key='edit'):
+        # st.sidebar.write(text)
+        # Read the text from the file
+        text = st.text_area('**Article**',text)
+        with open(f'podcast/{episode}/text/{article}', 'w', encoding='utf-8') as f:
+            f.write(text)
+        # st.sidebar.write(text)
+    else:
+        st.markdown(f'**Article**')
+        st.write(text)
+
     # Summary
     if not os.path.exists(f'podcast/{episode}/summary/{article}'):
         if st.button("Summary"):
             if not os.path.exists(f'podcast/{episode}/summary'):
                 os.mkdir(f'podcast/{episode}/summary')
             # Read the text from the file
-            with open(f'podcast/{episode}/text/{article}', 'r',encoding='utf-8') as f:
+            with open(f'podcast/{episode}/text/{article}', 'r', encoding='utf-8') as f:
                 text = f.read()
             summary = summarize(text)
             st.write(f'Summary characters: {len(summary)}')
@@ -58,13 +98,15 @@ if os.path.exists(directory):
             st.write("Summary generated.")
             st.write(summary)
     else:
-        with open(f'podcast/{episode}/summary/{article}', 'r',encoding='utf-8') as f:
+        st.markdown(f'**Summary**')
+        with open(f'podcast/{episode}/summary/{article}', 'r', encoding='utf-8') as f:
             summary = f.read()
         st.write(summary)
 
 
         # Audio
-        if os.path.exists(f'podcast/{episode}/audio/' + article.replace('.txt', '.mp3')): 
+        if os.path.exists(f'podcast/{episode}/audio/' + article.replace('.txt', '.mp3')):
+            st.markdown(f'**Audio**')
             with open(f'podcast/{episode}/audio/' + article.replace('.txt', '.mp3'), 'rb') as f:
                 audio = f.read()
             st.audio(audio)
