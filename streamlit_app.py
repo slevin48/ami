65b9781d8d918fa316026bce0a0c119adff0f442
Slevin48 <yann.debray@gadz.org>
2023-11-23 18:40:44 -0500

    Add password and sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import openai, boto3, re
 from io import BytesIO
 
+st.set_page_config(page_title='AMI',
+                   page_icon='🤖',
+                   initial_sidebar_state='expanded',)
+
 # Load environment variables
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 voices = ['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer']
@@ -60,75 +64,78 @@ def tts_s3(summary, article, episode, voice, bucket_name):
     s3_client.upload_fileobj(audio_data, bucket_name, speech_file_path)
 
 # Streamlit app
-st.title("AMI 🤖 Artificial Market Intelligence")
+st.sidebar.title("Conseil d'ami 🤖")
 
-# User input for episode number
-episode_number = st.number_input("Enter Episode Number", min_value=1, value=2, step=1)
-episode = 'episode' + '{:03d}'.format(episode_number)
+password = st.sidebar.text_input('Enter password', type='password')
+if password == st.secrets['PASSWORD']:
 
-# Create article
-if st.button("Create Article"):
-        st.session_state.create_article = True
-if st.session_state.create_article:
-    if title := st.text_input("Enter Article title", key='title'):
-        title = title.replace(':', '-').replace('Les Echos - ', '')
-        title = re.sub(r'[<>:"/\\|?*]', '', title)
-        create_article_s3(title, BUCKET_NAME)
-        st.session_state.create_article = False
+    # User input for episode number
+    episode_number = st.sidebar.number_input("Enter Episode Number", min_value=1, value=2, step=1)
+    episode = 'episode' + '{:03d}'.format(episode_number)
 
-# Display articles
-directory = episode
-articles = list_files_s3(BUCKET_NAME, directory + '/text')
-# st.write(articles)
-if articles:
-    article = st.selectbox("Select an Article", articles, format_func=lambda x: x.split('/')[-1].replace('.txt', ''))
-    article = article.split('/')[-1]
+    # Create article
+    if st.sidebar.button("Create Article"):
+            st.session_state.create_article = True
+    if st.session_state.create_article:
+        if title := st.sidebar.text_input("Enter Article title", key='title'):
+            title = title.replace(':', '-').replace('Les Echos - ', '')
+            title = re.sub(r'[<>:"/\\|?*]', '', title)
+            create_article_s3(title, BUCKET_NAME)
+            st.session_state.create_article = False
 
-    # Text
-    file_path = f'{episode}/text/{article}'
-    if file_path in articles:
-        text = read_file_s3(BUCKET_NAME, file_path)
-        if st.toggle("Edit Article", key='edit'):
-            text = st.text_area('**Article**', text)
-            write_file_s3(BUCKET_NAME, file_path, text)
+    # Display articles
+    directory = episode
+    articles = list_files_s3(BUCKET_NAME, directory + '/text')
+    # st.write(articles)
+    if articles:
+        article = st.sidebar.selectbox("Select an Article", articles, format_func=lambda x: x.split('/')[-1].replace('.txt', ''))
+        article = article.split('/')[-1]
+
+        # Text
+        file_path = f'{episode}/text/{article}'
+        if file_path in articles:
+            text = read_file_s3(BUCKET_NAME, file_path)
+            if st.sidebar.toggle("Edit Article", key='edit'):
+                text = st.text_area('**Article**', text)
+                write_file_s3(BUCKET_NAME, file_path, text)
+            else:
+                st.write(f'**Article:**',article.replace('.txt', ''))
+                st.write(text)
+
+        # Summary
+        summary_path = f'{episode}/summary/{article}'
+        summaries = list_files_s3(BUCKET_NAME, directory + '/summary')
+
+        if summary_path not in summaries:
+            if st.button("Summary"):
+                summary = summarize(text)
+                st.write(f'Summary characters: {len(summary)}')
+                write_file_s3(BUCKET_NAME, summary_path, summary)
+                st.write("Summary generated.")
+                st.write(summary)
         else:
-            st.write(f'**Article:**',article.replace('.txt', ''))
-            st.write(text)
-
-    # Summary
-    summary_path = f'{episode}/summary/{article}'
-    summaries = list_files_s3(BUCKET_NAME, directory + '/summary')
-
-    if summary_path not in summaries:
-        if st.button("Summary"):
-            summary = summarize(text)
-            st.write(f'Summary characters: {len(summary)}')
-            write_file_s3(BUCKET_NAME, summary_path, summary)
-            st.write("Summary generated.")
+            st.write(f'**Summary**')
+            summary = read_file_s3(BUCKET_NAME, summary_path)
             st.write(summary)
-    else:
-        st.write(f'**Summary**')
-        summary = read_file_s3(BUCKET_NAME, summary_path)
-        st.write(summary)
 
-        # Audio
-        audio_path = f'{episode}/audio/' + article.replace('.txt', '.mp3')
-        audios = list_files_s3(BUCKET_NAME, directory + '/audio')
-        if audio_path in audios:
-            st.write(f'**Audio**')
-            audio_url = s3_client.generate_presigned_url('get_object',
-                                                        Params={'Bucket': BUCKET_NAME, 'Key': audio_path},
-                                                        ExpiresIn=3600)
-            st.audio(audio_url)
-        else:
-            voice = st.radio("Select Voice", voices)
-            if st.button("TTS"):
-                tts_s3(summary, article, episode, voice, BUCKET_NAME)
-                st.write("Audio generated.")
+            # Audio
+            audio_path = f'{episode}/audio/' + article.replace('.txt', '.mp3')
+            audios = list_files_s3(BUCKET_NAME, directory + '/audio')
+            if audio_path in audios:
+                st.write(f'**Audio**')
                 audio_url = s3_client.generate_presigned_url('get_object',
                                                             Params={'Bucket': BUCKET_NAME, 'Key': audio_path},
                                                             ExpiresIn=3600)
                 st.audio(audio_url)
+            else:
+                voice = st.radio("Select Voice", voices)
+                if st.button("TTS"):
+                    tts_s3(summary, article, episode, voice, BUCKET_NAME)
+                    st.write("Audio generated.")
+                    audio_url = s3_client.generate_presigned_url('get_object',
+                                                                Params={'Bucket': BUCKET_NAME, 'Key': audio_path},
+                                                                ExpiresIn=3600)
+                    st.audio(audio_url)
 
-else:
-    st.write("No articles found for the selected episode.")
+    else:
+        st.write("No articles found for the selected episode.")
